04ae3b619a9dba1a0872d7d828dca3eb68f00811
AlexKirkouski <alex.kirkouski@gmail.com>
2025-10-28 11:27:52 +0000

    Small fix

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ WORKERS="${WORKERS:-2}"  # >= 2 чтобы не было дедлока на с�
 
 # Render runtime config from template
 sed "s/__MODEL_ID__/${MODEL_ID}/g" /app/litellm_config.template.yaml > /app/litellm_config.yaml
-exec litellm --port 4000 --config /app/litellm_config.yaml --workers "${WORKERS}"
+exec litellm --port 4000 --config /app/litellm_config.yaml --num_workers "${WORKERS}"
